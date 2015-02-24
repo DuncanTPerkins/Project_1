7099edc7f1bec7886f66cd8070d6fe57d27750eb
Chance Reichenberg <chance.reichenberg@gmail.com>
2015-02-24 16:40:12 -0500

    Words Class and Finished Distinct Words Class
    Completed DistinctWords.cs
    Almost Completed Words.cs. Need altered and completed Text.cs to finish.

--- Project1/Driver.cs
+++ Project1/Driver.cs
@@ -4,7 +4,7 @@
 //	File Name:		Driver.cs
 //	Description:    ?????
 //	Course:			CSCI 2210-001 - Data Structures
-//	Authors:		Chance Reichenberg, Duncan Perkins, perkinsdt@goldmail.etsu.edu, reichenberg@etsu.edu, Department of Computing, East Tennessee State University
+//	Authors:		Chance Reichenberg, reichenberg@etsu.edu, Duncan Perkins, perkinsdt@goldmail.etsu.edu,  Department of Computing, East Tennessee State University
 //	Created:	    Thursday, February 5, 2015
 //	Copyright:		Chance Reichenberg, Duncan Perkins 2015
 //
@@ -22,12 +22,14 @@ namespace Project1
                 [STAThread]
         static void Main(string[] args)
         {
-            Text test = new Text();
-            test.GetTokens();
-            foreach(var  i in test.Tokens)
-            {
-                Console.WriteLine(i);
-            }
+            //Text test = new Text();
+            //test.GetTokens();
+            //foreach(var  i in test.Tokens)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
 
             
             Console.ReadLine();
---
+++ Project1/Words.cs
@@ -0,0 +1,75 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Project:	    Project 1
+//	File Name:		Words.cs
+//	Description:    Holds a list of distinct words for sorting and displaying
+//	Course:			CSCI 2210-001 - Data Structures
+//	Author:			Chance Reichenberg, reichenberg@etsu.edu, Department of Computing, East Tennessee State University
+//	Created:	    Monday, February 23, 2015
+//	Copyright:		Chance Reichenberg, 2015
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    /// <summary>
+    /// Class used to display Distinct words found in a text file
+    /// </summary>
+    class Words
+    {
+        /// <summary>
+        /// Public property that holds the distinct words
+        /// </summary>
+        public List<DistinctWord> DistinctWords { get; private set; }
+
+        /// <summary>
+        /// Public read-only property that holds the number of distinct words
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return DistinctWords.Count;
+            }
+        }
+
+        /// <summary>
+        /// Default Constructor for the Words class
+        /// </summary>
+        public Words()
+        {
+            DistinctWords = null;
+        }
+
+        /// <summary>
+        /// Parameterized constructor that takes a text object and puts the tokens into the list of Distinct words.
+        /// </summary>
+        /// <param name="words">Text object containing File text</param>
+        public Words(Text words)
+        {
+            
+            DistinctWords.Sort();
+        }
+
+        /// <summary>
+        /// Method used to display Distinct words 
+        /// </summary>
+        public void Display()
+        {
+            int count = 0;
+
+            Console.WriteLine("  Word\tCount");
+            Console.WriteLine("  ----\t----");
+            foreach(var item in DistinctWords)
+            {
+                count++;
+                Console.WriteLine(count + ". " + item);
+            }
+        }
+    }
+}
